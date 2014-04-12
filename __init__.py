9de247616589ab4e138f8c5cd5134f345ce6af7d
Hussain Parsaiyan <hussain.parsaiyan@iceanimations.com>
2014-04-12 11:04:30 +0500

    bug that was overwriting saveImage because of pc.mel.eval removed. arnoldmenu import removed from __init__

--- __init__.py
+++ __init__.py
@@ -1,4 +1,3 @@
-import arnoldmenu
 import pymel.core as pc
 import os
 op = os.path
@@ -9,13 +8,14 @@ VER = pc.about(v = True)
 
 for ver in SPRT_FOR:
     if ver in VER:
-        renderWindowPanel = op.join(op.dirname(arnoldmenu.__file__ ),
+        
+        renderWindowPanel = op.join(op.dirname(__file__ ),
                                     'renderWindowPanel'+ '_' + ver + '.mel')
-        with open(renderWindowPanel, 'r') as r:
-            arnoldmenu_file = arnoldmenu.__file__.replace('\\', '/')
-            print arnoldmenu_file
-            if (arnoldmenu_file.endswith('.pyc') and
-                op.exists(arnoldmenu_file[:-1])):
-                arnoldmenu_file = arnoldmenu_file[:-1]
-            pc.mel.eval(r.read().replace(r'arnoldmenu',
-                                         arnoldmenu_file))
+        arnoldmenu_file = __file__.replace('\\', '/')
+        print arnoldmenu_file
+        if (arnoldmenu_file.endswith('.pyc') and
+            op.exists(arnoldmenu_file[:-1])):
+            arnoldmenu_file = arnoldmenu_file[:-1]
+        pc.mel.eval('global string $ice_arnold_aov_menu_gen = "%s";'
+                    %(op.dirname(arnoldmenu_file) + '/arnoldmenu.py'))
+        pc.mel.eval('source "%s";'%(renderWindowPanel.replace('\\', '/')))
