f157be2136d0a43c12675da9b22385ec5f0da331
Qurban <qurban_ali36@yahoo.com>
2015-01-08 17:50:07 +0500

    indentation corrected

--- arnoldmenu.py
+++ arnoldmenu.py
@@ -22,25 +22,24 @@ def aovUI():
 
 def loadAOV(*args):
 
-	activeAOVS = cmds.ls(et='aiAOV')
-	aovsNames = [i.split('_', 1)[1] for i in activeAOVS]
+    activeAOVS = cmds.ls(et='aiAOV')
+    aovsNames = [i.split('_', 1)[1] for i in activeAOVS]
 
-	if activeAOVS == []:
+    if activeAOVS == []:
             cmds.warning("No aov's setup")
-	else:
-
-            rview = cmds.getPanel( sty = 'renderWindowPanel' )
-            selectedAOV = cmds.optionMenu('com_iceanimations_arnold_aovs',q=1,v=1)
-            pc.setAttr("defaultArnoldRenderOptions.displayAOV", selectedAOV)
-            
-            img = pc.renderSettings(fin = True, lut = True, fpt = True)[0]
-            if '<RenderPass>' not in img:
-                gin = pc.renderSettings(gin = 1, lut =1)[0]
-                imgTillTmp = img[:img.find(gin)]
-                img = op.join(imgTillTmp, selectedAOV, gin)
-            pathToImg = img.replace('<RenderPass>', selectedAOV)
-            print pathToImg
-            cmds.renderWindowEditor(rview, e=True, li=pathToImg)
+    else:
+        rview = cmds.getPanel( sty = 'renderWindowPanel' )
+        selectedAOV = cmds.optionMenu('com_iceanimations_arnold_aovs',q=1,v=1)
+        pc.setAttr("defaultArnoldRenderOptions.displayAOV", selectedAOV)
+        
+        img = pc.renderSettings(fin = True, lut = True, fpt = True)[0]
+        if '<RenderPass>' not in img:
+            gin = pc.renderSettings(gin = 1, lut =1)[0]
+            imgTillTmp = img[:img.find(gin)]
+            img = op.join(imgTillTmp, selectedAOV, gin)
+        pathToImg = img.replace('<RenderPass>', selectedAOV)
+        print pathToImg
+        cmds.renderWindowEditor(rview, e=True, li=pathToImg)
 
 def drawMenu():
     if pc.mel.currentRenderer().lower() != "arnold":
